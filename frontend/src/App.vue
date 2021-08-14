cd9faecabb7ea1f8d18ddd81685b644403be6e22
Eduardo Peschke <eduardopeschke@gmail.com>
2021-08-13 23:06:01 -0400

    API request/response Working

---
+++ frontend/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    {{ data }}}
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      data: null
+    }
+  },
+  components: {
+  },
+  mounted() {
+    axios
+    .get('http://127.0.0.1:8000/api/v1/cards/')
+    .then(response => this.data = response.data)
+  }
+}
+</script>
+
+<style>
+</style>
