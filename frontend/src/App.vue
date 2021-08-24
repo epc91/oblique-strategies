417b4efed38fa8b0955466f84b4688b530151876
Eduardo Peschke <eduardopeschke@gmail.com>
2021-08-24 19:07:25 -0400

    Add Title/styles

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,95 +1,101 @@
 <template>
   <div id="app">
+    <div class="title">
+      <h1>Oblique Strategies</h1>
+    </div>
     <div class="card">
       <div class="text" @click="randomId()">
         {{ data }}
       </div>
     </div>
   </div>
-
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
-  name: 'App',
+  name: "App",
   data() {
     return {
       data: null,
       total: null,
       idCard: null,
-    }
+    };
   },
-  components: {
-  },
+  components: {},
   mounted() {
-    this.data = 'Click here to select a random card'
-    this.getTotal()
+    this.data = "Click here to select a random card";
+    this.getTotal();
   },
   methods: {
     // GET TOTAL
     getTotal() {
       axios({
-        method: 'get',
-        url: 'http://127.0.0.1:8000/api/v1/cards/',
-      })
-      .then(response => this.total = response.data['count'])
+        method: "get",
+        url: "http://127.0.0.1:8000/api/v1/cards/",
+      }).then((response) => (this.total = response.data["count"]));
     },
-      // GET CARD
-      getCard(id) {
-        axios({
-          method: 'get',
-          url: 'http://127.0.0.1:8000/api/v1/cards/' + id
-        })
-        .then(response => this.data = response.data['description'])
-      },
+    // GET CARD
+    getCard(id) {
+      axios({
+        method: "get",
+        url: "http://127.0.0.1:8000/api/v1/cards/" + id,
+      }).then((response) => (this.data = response.data["description"]));
+    },
     // RANDOM ID
     randomId() {
-      this.idCard = Math.floor((Math.random() * this.total) + 1)
-      this.getCard(this.idCard)
+      this.idCard = Math.floor(Math.random() * this.total + 1);
+      this.getCard(this.idCard);
     },
   },
-  
-}
+};
 </script>
 
 <style>
-
-  *, *::before, *::after {
-      box-sizing: border-box;
-  }
-  #app {
-    background-image: url('~@/assets/bg.png');
-    background-size: cover;
-    color: rgb(0, 0, 0);
-    font-family: sans-serif;
-    width: 100vw;
-    height: 100vh;
-    position: fixed;
-    top: 0;
-    left: 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-size: 25px;
-    text-align: center;
-  }
-  .card {
-    width: 9cm;
-    height: 7cm;
-    background-color: rgba(230, 230, 230, 0.7);
-    border-radius: 10px;
-    padding: 20px;
-    justify-content: center;
-    align-items: center;
-    display: flex;
-  }
-  .card:hover {
-    background-color: rgb(0, 0, 0);
-    color: rgba(204, 204, 204, 0.7);
-  }
-  .text {
-    cursor: pointer;
-  }
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+#app {
+  background-image: url("~@/assets/bg.png");
+  background-size: cover;
+  color: rgb(0, 0, 0);
+  font-family: sans-serif;
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 25px;
+  text-align: center;
+}
+.title {
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  color: azure;
+  margin: 0px 130px 0px 0px;
+}
+.card {
+  width: 9cm;
+  height: 7cm;
+  background-color: rgba(230, 230, 230, 0.7);
+  border-radius: 10px;
+  padding: 20px;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+}
+.card:hover {
+  background-color: rgb(0, 0, 0);
+  color: rgba(204, 204, 204, 0.7);
+}
+.text {
+  cursor: pointer;
+}
 </style>
